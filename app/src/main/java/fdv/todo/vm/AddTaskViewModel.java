6d3d243416fab9276b814a3fcdf30cc482a4929c
Nick <fabdadv@gmai.com>
2018-05-14 13:17:01 +0700

    AAC: adding ViewModel Factory for Task

--- app/src/main/java/fdv/todo/vm/AddTaskViewModel.java
+++ app/src/main/java/fdv/todo/vm/AddTaskViewModel.java
@@ -1,6 +1,20 @@
 package fdv.todo.vm;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
 
+import fdv.todo.data.db.TaskEntity;
+import fdv.todo.data.db.TasksDB;
+
+// (F.5) Make this class extend ViewModel
 public class AddTaskViewModel extends ViewModel {
+    // (F.6) Add a task member variable for the TaskEntry object wrapped in a LiveData
+    private LiveData<TaskEntity> task;
+    // (F.7) Create a constructor where you call loadTaskById of the taskDao to initialize the tasks variable
+    // Note: The constructor should receive the database and the taskId
+    public AddTaskViewModel(TasksDB database, int taskId) {
+        task = database.tasksDao().loadTaskById(taskId);
+    }
+    // (F.8) Create a getter for the task variable
+    public LiveData<TaskEntity> getTask() { return task; }
 }
--- app/src/main/java/fdv/todo/vm/AddTaskViewModelFactory.java
+++ app/src/main/java/fdv/todo/vm/AddTaskViewModelFactory.java
@@ -5,17 +5,17 @@ import android.arch.lifecycle.ViewModelProvider;
 
 import fdv.todo.data.db.TasksDB;
 
-// TODO (F.1) Make this class extend ViewModelAddTaskViewModelFactory ViewModelProvider.NewInstanceFactory
+// (F.1) Make this class extend ViewModelAddTaskViewModelFactory ViewModelProvider.NewInstanceFactory
 public class AddTaskViewModelFactory extends ViewModelProvider.NewInstanceFactory {
-    // TODO (F.2) Add two member variables. One for the database and one for the taskId
+    // (F.2) Add two member variables. One for the database and one for the taskId
     private final TasksDB db;
     private final int taskId;
-    // TODO (F.3) Initialize the member variables in the constructor with the parameters received
+    // (F.3) Initialize the member variables in the constructor with the parameters received
     public AddTaskViewModelFactory(TasksDB database, int taskId) {
         db = database;
-        taskId = taskId;
+        this.taskId = taskId;
     }
-    // TODO (F.4) Uncomment the following method
+    // (F.4) Uncomment the following method
     // Note: This can be reused with minor modifications
     @Override
     public <T extends ViewModel> T create(Class<T> modelClass) {
